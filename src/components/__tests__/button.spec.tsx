e4892991ab81214adc4ba2deb3122371963a61cd
cloudydong <cloudy12028@gmail.com>
2021-04-19 12:31:34 +0900

    app, button spec

---
+++ src/components/__tests__/button.spec.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { Button } from "../button";
+
+describe("<Button />", () => {
+  it("should render OK with props", () => {
+    const { getByText, rerender } = render(
+      <Button canClick={true} loading={false} actionText={"test"} />
+    );
+    getByText("test");
+    rerender(<Button canClick={true} loading={true} actionText={"test"} />);
+    getByText("...로딩중");
+  });
+});
